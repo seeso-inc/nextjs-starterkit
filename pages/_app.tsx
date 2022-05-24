3090350e8167be1065b8b10fb1845bf5ee064862
Jinui You <baram991103@gmail.com>
2022-05-24 13:56:07 +0900

    :sparkles: Add ga module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,13 @@
-import Head from 'next/head';
-import Script from 'next/script';
+import GoogleAnalytics from 'components/GoogleAnalytics';
 import Layout from 'components/Layout';
-import { useEffect } from 'react';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }) => {
 
   return (
     <>
       <Head>
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-K55Z454WYB"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-K55Z454WYB');
-            `}
-        </Script>
+        <GoogleAnalytics />
       </Head>
 
       <Layout>
